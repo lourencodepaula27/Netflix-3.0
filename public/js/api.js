40e5ef2a72558fa63c9824c93a58dd5ac42f1e50
lourencodepaula27 <lourenco.bjj@outlook.com>
2022-12-13 11:38:19 -0300

    Clone visual Netflix

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "7906cfc4fd66620c66e4f8205de4a007"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
